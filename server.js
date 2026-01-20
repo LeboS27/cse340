a10db07a98542e74f317136c406332258c208b53
Lebohang Sebata <lebohangsebata20@gmail.com>
2026-01-20 22:26:43 +0200

    robust error handling

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.set('layout', 'layouts/layout');
 // Routes
 app.use("/inv", inventoryRoute);
 app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -27,14 +29,15 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
-  });
-});
+  })
+})
 
 // Start server
 const PORT = process.env.PORT || 8080;
