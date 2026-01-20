9030eb22962c9d039fe92308867c4ac80b86e15d
Lebohang Sebata <lebohangsebata20@gmail.com>
2026-01-20 15:56:40 +0200

    bugs fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities");  // This import is REQUIRED based on the error handler
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
@@ -13,42 +14,30 @@ app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
 
 // Routes
-/*
-app.get('/', (req, res) => {
-    res.render('index', { 
-        title: 'Home | CSE Motors',
-        currentUrl: '/'
-    });
+app.use("/inv", inventoryRoute);
+app.get("/", baseController.buildHome);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'});
 });
-*/
-
-//index route
-// Inventory routes
-app.use("/inv", inventoryRoute)
-
-app.get("/", baseController.buildHome)
 
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
     nav
-  })
-})
-
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
-})
+  });
+});
 
 // Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
